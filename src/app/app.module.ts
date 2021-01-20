2318e7fefd7d2b4293815758e5df3b0cc14f5f1d
vijaya Bhaskar <vijayabhaskarreddyannapareddy@gmail.com>
2021-01-20 13:25:20 +0530

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,38 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { NotesService } from './notes.service';
 
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
-  declarations: [ ],
-  imports: [ ],
-  providers: [ ],
-  bootstrap: [ ]
+  declarations: [AppComponent,
+  HeaderComponent ],
+  imports: [BrowserModule, 
+  FormsModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  BrowserAnimationsModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatInputModule,
+  ReactiveFormsModule,
+  HttpClientModule,
+
+  MatCardModule
+ ],
+  providers: [NotesService ],
+  bootstrap: [AppComponent ]
 })
 export class AppModule { }
--- src/app/note.ts
+++ src/app/note.ts
@@ -1,2 +1,4 @@
 export class Note {
+    public title:any;
+    public text:any;
 }
